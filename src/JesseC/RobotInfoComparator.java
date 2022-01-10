070bb6d1a92215addc5f069a6b4aaa8fbe4a81a9
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2022-01-10 18:30:48 -0500

    Update RobotInfoComparator.java

--- src/JesseC/RobotInfoComparator.java
+++ src/JesseC/RobotInfoComparator.java
@@ -1,12 +1,1 @@
-package JesseC;
 
-import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
-
-import java.util.Comparator;
-
-public class RobotInfoComparator implements Comparator<RobotInfo>{
-    public int compare(RobotInfo a, RobotInfo b){
-        return a.getHealth() - b.getHealth();
-    }
-}
